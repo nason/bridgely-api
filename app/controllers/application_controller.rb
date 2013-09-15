8048d8f816af9008472aa0274efb667501b64140
Michael Nason <m.s.nason@gmail.com>
2013-09-14 18:45:24 -0700

    Add comment about why these includes are in actioncontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  # These are required to get rails-api to work correctly with strong parameters
   include ActionController::ParamsWrapper
   include ActionController::StrongParameters
 end
