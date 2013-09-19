3e60d57bb2451a7e4691c07cd13c97f0408d434a
Michael Nason <m.s.nason@gmail.com>
2013-09-18 18:07:58 -0700

    company model validates presence of account_sid

--- app/models/v1/admin/company.rb
+++ app/models/v1/admin/company.rb
@@ -1,12 +1,17 @@
 class V1::Admin::Company < ActiveRecord::Base
 
-  #Validations
-  validates :name, :presence => true
-  validates :name, :uniqueness => true
+  # Validations
+  validates :name, presence: true
+  validates :name, uniqueness: true
 
-  #Associations
+  validates :account_sid, presence: true
+
+  # Associations
   has_many :users
   has_many :employees
   has_many :messages, through: :employees
+
+  # Serialize settings
+  serialize :settings, Hash
 
 end
