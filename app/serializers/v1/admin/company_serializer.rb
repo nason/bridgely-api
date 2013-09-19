2337be600b79790ba3f842b4ec99b78daf56adf4
Michael Nason <m.s.nason@gmail.com>
2013-09-19 12:13:57 -0700

    Add company sid to serializer

--- app/serializers/v1/admin/company_serializer.rb
+++ app/serializers/v1/admin/company_serializer.rb
@@ -1,4 +1,4 @@
 class V1::Admin::CompanySerializer < ActiveModel::Serializer
-  attributes :id, :name, :settings
+  attributes :id, :name, :account_sid, :settings
   # has_many :users, embed :id
 end
