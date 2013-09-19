40545c7df21ef20d432fa43334e79d437fcaef1c
Michael Nason <m.s.nason@gmail.com>
2013-09-19 11:52:09 -0700

    Update queries in twilio controller

--- app/controllers/v1/twilio_controller.rb
+++ app/controllers/v1/twilio_controller.rb
@@ -10,12 +10,12 @@ class V1::TwilioController < ApplicationController
   def create
 
     # Find the company associated with the AccountSid
-    @company = V1::Admin::Company.find_by_account_sid( twilio_params[:AccountSid] )
+    @company = V1::Admin::Company.find_by account_sid: twilio_params[:AccountSid]
 
     if @company
 
       # Find or the employee associated with the company_id and phone number
-      @employee = V1::Employee.find_by_phone( twilio_params[:phone] ).where( :company_id => @company.id )
+      @employee = V1::Employee.find_by phone: twilio_params[:phone], company_id: @company.id
 
       # Create the employee if record does not exist
       @employee = V1::Employee.create({
@@ -46,7 +46,7 @@ class V1::TwilioController < ApplicationController
   def update
 
     # Find the message by SID
-    @message = V1::Message.find_by_message_sid( twilio_params[:MessageSid] )
+    @message = V1::Message.find_by message_sid: twilio_params[:MessageSid]
 
     # Update the status
     @message.update_attribute( :status, twilio_params[:SmsStatus] )
