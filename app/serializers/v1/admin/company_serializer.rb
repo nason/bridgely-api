e46a52fe515ee26bfda675a3959a94c2e86c6674
Michael Nason <m.s.nason@gmail.com>
2013-09-23 17:09:29 -0700

    Update user and company serializers

--- app/serializers/v1/admin/company_serializer.rb
+++ app/serializers/v1/admin/company_serializer.rb
@@ -1,4 +1,5 @@
 class V1::Admin::CompanySerializer < ActiveModel::Serializer
-  attributes :id, :name, :account_sid, :settings
+  attributes :id, :name, :settings
+  # removed :account_sid from above
   # has_many :users, embed :id
 end
--- app/serializers/v1/admin/user_serializer.rb
+++ app/serializers/v1/admin/user_serializer.rb
@@ -1,4 +1,9 @@
 class V1::Admin::UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :admin, :authorization_token
   has_one :company
+
+  def include_admin?
+    # Only include the admin property for admins
+    self.admin
+  end
 end
