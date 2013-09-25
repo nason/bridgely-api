8c5cc2f824522dbe67096b794be35adc276310aa
Michael Nason <m.s.nason@gmail.com>
2013-09-25 11:45:21 -0700

    Update message serializer

--- app/serializers/v1/message_serializer.rb
+++ app/serializers/v1/message_serializer.rb
@@ -1,3 +1,6 @@
 class V1::MessageSerializer < ActiveModel::Serializer
-  attributes :id, :body, :direction, :company_id, :question_id, :employee_ids
+  # attributes :id, :body, :direction, :company_id, :question_id, :employee_ids
+  attributes :id, :body, :direction, :company_id
+  has_one :question, embed: :id
+  has_many :employees, embed: :ids
 end
