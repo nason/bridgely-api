935ac6d98f0604346048807dc41fedd01cbb3556
Michael Nason <m.s.nason@gmail.com>
2013-09-27 16:52:51 -0700

    Add message to question serializer

--- app/serializers/v1/question_serializer.rb
+++ app/serializers/v1/question_serializer.rb
@@ -1,5 +1,3 @@
 class V1::QuestionSerializer < ActiveModel::Serializer
-  # attributes :id, :body, :direction, :company_id, :question_id, :employee_ids
-  attributes :id, :title, :response_tag
-
+  attributes :id, :title, :message
 end
