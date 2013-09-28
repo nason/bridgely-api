3d7c870b7731f5dc927e71d1dc1a82a1bc583907
Michael Nason <m.s.nason@gmail.com>
2013-09-27 17:22:48 -0700

    Add nested users to company serializer

--- app/serializers/v1/admin/company_serializer.rb
+++ app/serializers/v1/admin/company_serializer.rb
@@ -1,5 +1,5 @@
 class V1::Admin::CompanySerializer < ActiveModel::Serializer
-  attributes :id, :name, :settings
+  attributes :id, :name, :settings, :users
   # removed :account_sid from above
 
   # has_many :users, embed :id
